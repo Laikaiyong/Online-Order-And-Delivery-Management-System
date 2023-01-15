60fa621dd86ff83ab8de86b876d3b47a262c118a
Laikaiyong <vandycklai@gmail.com>
2023-01-15 22:59:01 +0800

    Update progress

--- src/oodms/Oodms.java
+++ src/oodms/Oodms.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package oodms;
 
-/**
- *
- * @author USER
- */
 public class Oodms {
-
     /**
      * @param args the command line arguments
      */
---
+++ src/oodms/model/Admin.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodms.model;
+
+/**
+ *
+ * @author USER
+ */
+public class Admin extends User{
+    
+    
+//User Management (Add/Modify/search/delete Admin & Delivery Staff)  
+//
+//Category Management (Add/Modify/search/delete Category) 
+//
+//Item Management (Add Item Category-wise/Modify/search/delete Item) 
+//
+//Display All records of  
+//
+//Item Category  
+//
+//Items, Category-wise 
+//
+//Customer Orders 
+//
+//Customer Payment 
+//
+//Search Specific record of 
+//
+//Customer Order 
+//
+//Customer Payment 
+//
+//Order Delivery Management 
+//
+//Assign orders to Delivery Staff 
+//
+//Exit  
+}
